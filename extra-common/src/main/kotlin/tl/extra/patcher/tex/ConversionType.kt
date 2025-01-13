9d6e98d28db56ef9db76006e0d90cae82a7453f1
kotcrab <4594081+kotcrab@users.noreply.github.com>
2025-01-13 17:17:21 +0100

    Add unswizzled custom paletted tex conv

--- extra-common/src/main/kotlin/tl/extra/patcher/tex/ConversionType.kt
+++ extra-common/src/main/kotlin/tl/extra/patcher/tex/ConversionType.kt
@@ -4,17 +4,22 @@ import java.io.File
 
 enum class ConversionType {
   UNSWIZZLED_4BPP_PALETTED_NO_CCT,
+  UNSWIZZLED_4BPP_CUSTOM_PALETTED_NO_CCT,
   SWIZZLED_4BPP_PALETTED_LINEAR_CCT,
   SWIZZLED_4BPP_CUSTOM_PALETTED_LINEAR_CCT,
   SWIZZLED_4BPP_PALETTED_LINEAR_CCT_CORRECT_ALPHA,
   SWIZZLED_8BPP_PALETTED_LINEAR_CCT,
-  SWIZZLED_8BPP_CUSTOM_PALETTED_LINEAR_CCT;
+  SWIZZLED_8BPP_CUSTOM_PALETTED_LINEAR_CCT,
+  ;
 
   fun convert(origBytes: ByteArray, pngFile: File): ByteArray {
     println("Processing ${pngFile.absolutePath}...")
     return when (this) {
       UNSWIZZLED_4BPP_PALETTED_NO_CCT -> {
         PngToUnswizzled4BppPalettedTex(origBytes, pngFile).getTexBytes()
+      }
+      UNSWIZZLED_4BPP_CUSTOM_PALETTED_NO_CCT -> {
+        PngToUnswizzled4BppCustomPalettedTex(origBytes, pngFile).getTexBytes()
       }
       SWIZZLED_4BPP_PALETTED_LINEAR_CCT -> {
         PngToSwizzled4BppPalettedLinearCctTex(origBytes, pngFile).getTexBytes()
---
+++ extra-common/src/main/kotlin/tl/extra/patcher/tex/PngToUnswizzled4BppCustomPalettedTex.kt
@@ -0,0 +1,112 @@
+package tl.extra.patcher.tex
+
+import kio.KioInputStream
+import kio.KioOutputStream
+import tl.tex.ColorPalette
+import tl.tex.ImageReader
+import tl.util.findClosetsDivisibleBy
+import java.io.ByteArrayOutputStream
+import java.io.File
+import javax.imageio.ImageIO
+
+@Suppress("UNUSED_VARIABLE")
+class PngToUnswizzled4BppCustomPalettedTex(origBytes: ByteArray, pngFile: File) {
+  private val outStream = ByteArrayOutputStream()
+
+  init {
+    with(KioInputStream(origBytes)) {
+      val headerTop = readBytes(0x12) // this will never change
+      val swizzled = readShort().toInt()
+      val width = readShort().toInt()
+      val height = readShort().toInt()
+      val unkWidth = readShort().toInt()
+      val unkHeight = readShort().toInt()
+      val texDataSize = readInt()
+      val texOffset = readInt()
+      val unkCCTPresent = readInt()
+      val unkB2 = readInt()
+      val unkB3 = readInt()
+
+      if (swizzled == 1) {
+        println("WARN: This texture was originally swizzled")
+      }
+      if (unkCCTPresent == 1) {
+        println("WARN: This texture originally contained CCT")
+      }
+
+      setPos(0x30)
+      readBytes(texDataSize)
+
+      val newImage = ImageIO.read(pngFile)
+
+      val palettePos = pos()
+      val paletteHeader = readBytes(0x10)
+      val palMode = readShort(at = palettePos).toInt()
+      val palSize = readShort(at = palettePos + 0x4).toInt()
+      val palBytes = readBytes(palSize)
+
+      val newPalBytes = run {
+        val reader = ImageReader(newImage)
+        val colorsSet = mutableSetOf<Int>()
+        while (!reader.eof()) {
+          colorsSet.add(reader.nextPixel())
+        }
+        if (colorsSet.size > 16) {
+          error("Too much colors after reading image: ${colorsSet.size}")
+        }
+        val out = KioOutputStream(
+          ByteArrayOutputStream(),
+          littleEndian = false,
+        )
+        colorsSet.forEach {
+          out.writeInt((it shl 8) or (it ushr 24))
+        }
+        while (out.pos() < 64) {
+          out.writeInt(0)
+        }
+        out.getAsByteArrayOutputStream().toByteArray()
+      }
+      val palette = ColorPalette(newPalBytes, ColorPalette.Mode.RGBA8888)
+
+      val newImageDataWidth = findClosetsDivisibleBy(newImage.width, 32)
+      val newImageDataHeight = findClosetsDivisibleBy(newImage.height, 16)
+      val newImageReader = ImageReader(newImage, newImageDataWidth, newImageDataHeight)
+      val newTexDataSize = newImageDataWidth * newImageDataHeight / 2
+
+      val newTexBytes = ByteArrayOutputStream()
+      repeat(newTexDataSize) {
+        val color1 = newImageReader.nextPixel()
+        val color2 = newImageReader.nextPixel()
+        val colorId1 = palette.colors.indexOfFirst { it == color1 }
+        val colorId2 = palette.colors.indexOfFirst { it == color2 }
+        val encoded = (colorId2 shl 4) or colorId1
+        newTexBytes.write(encoded)
+      }
+
+      with(KioOutputStream(outStream)) {
+        writeBytes(headerTop)
+        writeShort(0) // not swizzled
+        writeShort(newImage.width.toShort())
+        writeShort(newImageDataHeight.toShort())
+        writeShort(newImageDataWidth.toShort())
+        writeShort(0) // unkHeight
+        writeInt(newTexDataSize)
+        writeInt(0) // texOffset
+        writeInt(0) // unkCCTPresent
+        writeInt(unkB2) // useless
+        writeInt(unkB3) // useless
+        writeBytes(newTexBytes.toByteArray())
+        writeShort(3)
+        writeShort(16)
+        writeInt(64)
+        writeInt(0)
+        writeInt(0) // paletteOffset?
+        writeBytes(newPalBytes)
+      }
+    }
+  }
+
+  fun getTexBytes(): ByteArray {
+    return outStream.toByteArray()
+  }
+}
